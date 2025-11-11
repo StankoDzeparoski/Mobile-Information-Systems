b436d54480e6a373ad0201ac138cddb1693855af
Stanko Dzeparoski <stankodzepar@gmail.com>
2025-11-12 00:37:15 +0100

    first commit

---
+++ lab1_exam_list/android/app/src/main/kotlin/com/example/lab1_exam_list/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lab1_exam_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lab1_exam_list/lib/models/exam.dart
@@ -0,0 +1,7 @@
+class Exam {
+  String name;
+  DateTime date;
+  List<String> rooms;
+
+  Exam({required this.name, required this.date, required this.rooms});
+}
